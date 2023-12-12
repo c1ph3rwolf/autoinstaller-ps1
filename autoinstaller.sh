b6d7d363b2d4ea4e61db1e3a42122afd7b7e8859
c1ph3rwolf <c1ph3rwolf@gmail.com>
2023-12-12 07:34:05 +0530

    added new bash script

---
+++ autoinstaller.sh
@@ -0,0 +1,14 @@
+#sudo apt-get update -y
+sudo apt update -y    
+sudo apt list --upgradable
+sudo apt update && sudo apt upgrade -y
+
+#sudo apt install kali-desktop-gnome
+#sudo apt install cpu-x -y 
+sudo apt install leafpad -y
+#sudo apt install snapd -y
+#sudo apt install gimp -y
+
+# Wifite Packages
+sudo apt install hcxdumptool -y
+sudo apt install hcxtools -y
